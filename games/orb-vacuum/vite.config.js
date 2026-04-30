6c5092c1b4ce68a3a50a0709315f6ad5e80467d9
george <george.allakhverdyan@gmail.com>
2026-04-29 21:12:41 -0700

    fix: deploy manual fix

--- games/orb-vacuum/vite.config.js
+++ games/orb-vacuum/vite.config.js
@@ -2,8 +2,11 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: 'src',
+  base: '/tech9/orb-vacuum/',
+  publicDir: false,
   build: {
     outDir: '../dist',
     emptyOutDir: true,
+    assetsDir: '_chunks',
   },
 });
